1977084fc8e58fc9cf927b942de45e673efcd9ee
beertwanger <steve@evolvere-tech.co.uk>
2023-12-15 17:00:27 +0000

    Colours dont work in kriten.

--- xmas/xmas.py
+++ xmas/xmas.py
@@ -2,15 +2,6 @@ import os
 import json
 import pyfiglet
 from rich.console import Console
-import logging
-from rich.logging import RichHandler
-
-FORMAT = "%(message)s"
-logging.basicConfig(
-    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
-)
-
-log = logging.getLogger("rich")
 
 console = Console()
 extra_vars = os.environ.get('EXTRA_VARS')
@@ -28,6 +19,6 @@ if name:
     msg_name = pyfiglet.figlet_format(name)
 
 #console.print(msg_xmas_in_colour)
-log.info(f'[red]{msg_xmas}', extra={"markup": True})
+console.print(f'[red]{msg_xmas}')
 if name:
-    log.info(f'[green]{msg_name}', extra={"markup": True})
+    console.print(f'[green]{msg_name}')
