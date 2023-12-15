0652588d0d3907bb9119bae6bd3758d955cf4741
JimmyWong974 <112889419+JimmyWong974@users.noreply.github.com>
2023-12-15 12:04:29 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI for your region
+  ami           = "ami-0666c668000b91fcb" # Replace with a valid AMI for your region
   instance_type = "t2.micro"
 
   tags = {
