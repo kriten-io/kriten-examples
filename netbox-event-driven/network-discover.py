719fc739b59abcc2f75cb63a0a191a909a8435f3
Marco Santonastaso <marco@santonastaso.com>
2024-04-30 14:10:29 +0100

    netbox-event-driven device_role updated

--- netbox-event-driven/network-discover.py
+++ netbox-event-driven/network-discover.py
@@ -66,7 +66,7 @@ class DiscoverNetwork():
                     # location = nb.dcim.locations.get(name='location').id,
                     # rack = nb.dcim.racks.get(facility_id=1).id,
                     device_type = nb.dcim.device_types.get(slug='server').id,
-                    device_role = 1,
+                    device_role = 2,
                     # device_role = nb.dcim.device_role.get(name='norole').id,
                     status='active',
                 )
